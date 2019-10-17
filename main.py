3f5e284d9f8028d4cd8725e4a19e1ca562164414
Jeff Vincent <jeff.d.vincent@gmail.com>
2019-10-16 23:33:37 -0700

    File upload to mongo works

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ from flask import Flask, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config['MONGO_URI'] = 'mongodb+srv://jeff:AsspWord@cluster0-intvw.mongodb.net/test?retryWrites=true&w=majority'
+app.config['DEBUG'] = True
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return "Media Server is up."
+    return """
+    <form action="/upload" method="post" enctype="multipart/form-data">
+        <p><input type=text name=filename>
+        <p><input type=file name=file>
+        <p><input type=submit value="upload file">
+    </form>
+    """
 
 @app.route('/upload', methods=['POST'])
 def upload():
     filename = request.form['filename']
-    return PyMongo.save_file(filename)
+    file = request.files['file']
+    return mongo.save_file(filename, file)
 
 @app.route('/download', methods=['POST'])
 def download():
     filename = request.form['filename']
-    return PyMongo.send_file(filename)
+    return mongo.send_file(filename)
 
 if __name__ == '__main__':
     app.run()
